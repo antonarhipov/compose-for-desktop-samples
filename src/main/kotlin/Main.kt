d6568f25f94c392b6dcfa67bda04658f63400cb5
anton <ant.arhipov@gmail.com>
2022-06-10 13:46:41 +0300

    key presses to clear and randomize background color

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,15 +8,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.isAltPressed
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.input.pointer.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -78,26 +84,49 @@ class Circle(
             color = color,
             center = offset,
             radius = 50f,
-            style = Stroke(width = 2.dp.toPx(), cap = StrokeCap.Round, join = StrokeJoin.Round)
+            style = Stroke(
+                width = 2.dp.toPx(),
+                cap = StrokeCap.Round,
+                join = StrokeJoin.Round,
+                pathEffect = PathEffect.dashPathEffect(FloatArray(10) {
+                    (0..10).random().toFloat()
+                })
+            )
         )
     }
 }
 
-
+var motionEvent by mutableStateOf(ACTION_IDLE)
+var backgroundColor by mutableStateOf(randomBackgroundColor())
 val shapes = mutableListOf<Shape>()
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Compose for Desktop",
-        state = rememberWindowState(width = 500.dp, height = 500.dp)
+        state = rememberWindowState(width = 500.dp, height = 500.dp),
+        onKeyEvent = {
+            if (it.key == Key.C) {
+                shapes.clear()
+                motionEvent = ACTION_IDLE
+            }
+            if (it.key == Key.B) {
+                backgroundColor = randomBackgroundColor()
+            }
+            true
+        }
     ) {
         MaterialTheme {
-            var motionEvent by remember { mutableStateOf(ACTION_IDLE) }
+
             var currentPosition by remember { mutableStateOf(Offset.Unspecified) }
             Canvas(
                 modifier = Modifier.fillMaxSize()
-                    .background(Color.LightGray)
+                    .background(backgroundColor)
+                    .onKeyEvent {
+                        println("${it.key} is pressed")
+                        true
+                    }
                     .pointerInput(Unit) {
                         forEachGesture {
                             awaitPointerEventScope {
@@ -128,7 +157,9 @@ fun main() = application {
                 }
 
                 when (motionEvent) {
-                    ACTION_DOWN -> redraw()
+                    ACTION_DOWN -> {
+                        redraw()
+                    }
                     ACTION_MOVE -> {
                         if (currentPosition != Offset.Unspecified) {
                             redraw()
@@ -146,14 +177,21 @@ fun main() = application {
     }
 }
 
-fun randomShape(currentPosition: Offset): Shape =
+fun randomShape(currentPosition: Offset): Shape = //Circle(randomColor(), currentPosition)
     when ((0..50).random()) {
         in 0..30 -> Circle(randomColor(), currentPosition)
         in 31..45 -> Square(randomColor(), currentPosition, randomRotation())
         else -> Ellipse(randomColor(), currentPosition, randomRotation())
     }
 
-fun randomColor() : Color {
+fun randomBackgroundColor(): Color {
+    val colorRange = 0..255
+    val r = colorRange.random()
+    val g = colorRange.random()
+    val b = colorRange.random()
+    return Color(r, g, b, 50)
+}
+fun randomColor(): Color {
     val colorRange = 0..255
     val r = colorRange.random()
     val g = colorRange.random()
