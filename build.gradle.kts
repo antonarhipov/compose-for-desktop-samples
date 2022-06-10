6f212c449b0574b339a2f823d568843371d9dfe3
anton <ant.arhipov@gmail.com>
2022-06-10 09:56:05 +0300

    corrected Gradle settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,36 @@
+import org.jetbrains.compose.compose
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.0-RC"
-    application
+    kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.compose") version "1.1.0"
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(compose.desktop.currentOs)
 }
 
-tasks.test {
-    useJUnitPlatform()
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions {
+        freeCompilerArgs = listOf(
+            "-Xcontext-receivers",
+            "-Xuse-k2",
+            "-Xbackend-threads=4",
+        )
+        jvmTarget = "11"
+        languageVersion = "1.7"
+    }
 }
 
-application {
-    mainClass.set("MainKt")
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
 
 rootProject.name = "composed"
 
