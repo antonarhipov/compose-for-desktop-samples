5b59819b839a3ad20ead1d0311e0f439d65fe777
anton <ant.arhipov@gmail.com>
2023-11-06 21:00:01 +0200

    kotlinizing the code

--- src/main/kotlin/midi/MidiSynthesizer.kt
+++ src/main/kotlin/midi/MidiSynthesizer.kt
@@ -11,29 +11,21 @@ private val SYNTH_PROP_KEY = "javax.sound.midi.Synthesizer"
 
 fun main() {
 
-    // Get a transmitter and synthesizer from their device names
-    // using system properties or defaults
     val trans = transmitter
     val synth = synthesizer
 
-    // The synthesizer is your MIDI device, which needs to be opened
     if (!synth.isOpen) {
         synth.open()
         println("Loaded instruments:")
         println(synth.loadedInstruments.contentToString())
     }
 
-    // You get your receiver from the synthesizer, then set it in
-    // your transmitter.  Optionally, you can create an implementation
-    // of Receiver to display the messages before they're sent.
     val receiver = synth.receiver
 //        val message = ShortMessage()
 //        receiver.send(message, 10000)
     val logger = MidiLogger(receiver) // optional
-    trans.receiver = logger // or just "receiver"
+    trans.receiver = logger
 
-    // You should be able to play on your musical keyboard (transmitter)
-    // and hear sounds through your PC synthesizer (receiver)
     println("Play on your musical keyboard...")
 
 }
@@ -77,16 +69,10 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
         val status = message.getStatus()
 
         // These statuses clutter the display
-        if (status == 0xf8) {
-            return
-        } // ignore timing messages
-        if (status == 0xfe) {
-            return
-        } // ignore status active
-        System.out.printf(
-            "%d - Status: 0x%s",
-            timeStamp, Integer.toHexString(status)
-        )
+        if (status == 0xf8) return // ignore timing messages
+        if (status == 0xfe) return // ignore status active
+
+        println("$timeStamp - Status: 0x${Integer.toHexString(status)}")
 
         // Strip channel number out of status
         val leftNibble = status and 0xf0
@@ -106,7 +92,6 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
         }
     }
 
-    // Displays raw data as integers, if any
     private fun displayRawData(message: MidiMessage) {
         val bytes = message.getMessage()
         if (message.length > 1) {
@@ -118,11 +103,6 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
         }
     }
 
-    // Display status and data of a NoteOn message.  Data may come
-    // in pairs after the status byte.
-    //
-    // Note that a NoteOn with a velocity of 0 is synonymous with
-    // a NoteOff message.
     private fun displayNoteOn(message: MidiMessage) {
         if (message.length < 3 || message.length % 2 == 0) {
             println(" Bad MIDI message")
@@ -136,9 +116,9 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
         } else {
             print(" = Note on")
         }
-        print(", Channel " + midiChannelToInt(message))
+        print(", Channel ${midiChannelToInt(message)}")
         if (bytes[2].toInt() == 0) {
-            println(", Note " + byteToInt(bytes[1]))
+            println(", Note ${byteToInt(bytes[1])}")
             return
         }
         print("\n\t")
@@ -147,16 +127,12 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
             if (i > 1) {
                 print("; ")
             }
-            System.out.printf(
-                "Number %d, Velocity %d",
-                byteToInt(bytes[i]), byteToInt(bytes[i + 1])
-            )
+            println("Number ${byteToInt(bytes[i])}, Velocity ${byteToInt(bytes[i + 1])}")
             i += 2
         }
         println()
     }
 
-    // Display status and data of a NoteOff message.
     private fun displayNoteOff(message: MidiMessage) {
         if (message.length < 3 || message.length % 2 == 0) {
             println(" Bad MIDI message")
@@ -170,17 +146,12 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
         }
     }
 
-    // Display status and data of a ControllerChange message.  Data may come
-    // in pairs after the status byte.
     private fun displayControllerChange(message: MidiMessage) {
         if (message.length < 3 || message.length % 2 == 0) {
             println(" Bad MIDI message")
             return
         }
-        print(
-            " = Controller Change, Channel "
-                    + midiChannelToInt(message) + "\n\t"
-        )
+        println(" = Controller Change, Channel ${midiChannelToInt(message)}")
         val bytes = message.getMessage()
         var i = 1
         while (i < message.length) {
@@ -196,17 +167,12 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
         println()
     }
 
-    // Display status and data of a KeyPressure message.  Data may come
-    // in pairs after the status byte.
     private fun displayKeyPressure(message: MidiMessage) {
         if (message.length < 3 || message.length % 2 == 0) {
             println(" Bad MIDI message")
             return
         }
-        print(
-            " = Key Pressure, Channel "
-                    + midiChannelToInt(message) + "\n\t"
-        )
+        println(" = Key Pressure, Channel ${midiChannelToInt(message)}")
         val bytes = message.getMessage()
         var i = 1
         while (i < message.length) {
@@ -222,17 +188,12 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
         println()
     }
 
-    // Display status and data of a PitchBend message.  Data may come
-    // in pairs after the status byte.
     private fun displayPitchBend(message: MidiMessage) {
         if (message.length < 3 || message.length % 2 == 0) {
             println(" Bad MIDI message")
             return
         }
-        print(
-            (" = Pitch Bend, Channel "
-                    + midiChannelToInt(message) + "\n\t")
-        )
+        println(" = Pitch Bend, Channel ${midiChannelToInt(message)}")
         val bytes = message.getMessage()
         var i = 1
         while (i < message.length) {
@@ -248,35 +209,27 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
         println()
     }
 
-    // Display status and data of a ProgramChange message
     private fun displayProgramChange(message: MidiMessage) {
         if (message.length < 2) {
             println(" Bad MIDI message")
             return
         }
-        print(
-            (" = Program Change, Channel "
-                    + midiChannelToInt(message) + "\n\t")
-        )
+        println(" = Program Change, Channel ${midiChannelToInt(message)}")
         val bytes = message.getMessage()
         for (i in 1 until message.length) {
             if (i > 1) {
                 print(", ")
             }
-            println("Program Number " + byteToInt(bytes[i]))
+            println("Program Number ${byteToInt(bytes[i])}")
         }
     }
 
-    // Display status and data of a ChannelPressure message
     private fun displayChannelPressure(message: MidiMessage) {
         if (message.length < 2) {
             println(" Bad MIDI message")
             return
         }
-        print(
-            (" = Channel Pressure, Channel "
-                    + midiChannelToInt(message) + "\n\t")
-        )
+        println(" = Channel Pressure, Channel ${midiChannelToInt(message)}")
         val bytes = message.getMessage()
         for (i in 1 until message.length) {
             if (i > 1) {
@@ -286,10 +239,6 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
         }
     }
 
-    // Display system messages.  Some may have data.
-    //
-    // "Begin System Exclusive" stops data interpretation, "End of
-    // System Exclusive" starts it again
     private fun displaySystemMessage(message: MidiMessage) {
         val bytes = message.getMessage()
         when (message.getStatus()) {
@@ -301,38 +250,26 @@ private class MidiLogger(private val receiver: Receiver) : Receiver {
             0xf1 -> if (bytes.size < 2) {
                 println(" Bad Data")
             } else {
-                println(
-                    (" = MIDI Time Code 1/4 Frame, Time Code "
-                            + byteToInt(bytes[1]))
-                )
+                println(" = MIDI Time Code 1/4 Frame, Time Code ${byteToInt(bytes[1])}")
             }
 
             0xf2 -> {
                 if (bytes.size < 3) {
                     println(" Bad Data")
                 } else {
-                    println(
-                        (" = Song Position, Pointer "
-                                + bytesToInt(bytes[1], bytes[2]))
-                    )
+                    println(" = Song Position, Pointer ${bytesToInt(bytes[1], bytes[2])}")
                 }
                 if (bytes.size < 2) {
                     println(" Bad Data")
                 } else {
-                    println(
-                        (" = Song Select, Song "
-                                + byteToInt(bytes[1]))
-                    )
+                    println(" = Song Select, Song ${byteToInt(bytes[1])}")
                 }
             }
 
             0xf3 -> if (bytes.size < 2) {
                 println(" Bad Data")
             } else {
-                println(
-                    (" = Song Select, Song "
-                            + byteToInt(bytes[1]))
-                )
+                println(" = Song Select, Song ${byteToInt(bytes[1])}")
             }
 
             0xf6 -> println(" = Tune Request")
